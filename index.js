1e9bd0831dfb481c6c2c4e101256738c780a18ac
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-03-21 08:49:00 +0300

    Added Kelvins

--- index.js
+++ index.js
@@ -5,16 +5,42 @@ const readlineInstance = readline.createInterface({
   output: process.stdout
 });
 
-readlineInstance.question("Please, enter degrees Celsius: ", answer => {
-  const answerParts = answer.split(",");
-  const restoredAnswer = answerParts.join(".");
-  const celsiusValue = Number(restoredAnswer);
+readlineInstance.question("Please, enter degrees: ", answer => {
+  const trimedAnswer = answer.replace(/\s+/g, " ").trim();
+  const answerParts = trimedAnswer.split(" ");
+  const answerNumPart = answerParts[0].split(",");
+  const restoredNumPart = answerNumPart.join(".");
+  const answerValue = parseFloat(restoredNumPart);
 
-  if (celsiusValue || celsiusValue === 0) {
-    const vahrenheitValue = celsiusValue * 1.8 + 32;
-    console.log("Fahrenheit value is ", vahrenheitValue);
+  if (answerValue || answerValue === 0) {
+    const answerStrPart = answerParts[1];
+
+    if (answerStrPart) {
+      const typeOfDegrees = answerStrPart.toLowerCase();
+
+      if (typeOfDegrees === "c") {
+        const fahrenheitValue = answerValue * 1.8 + 32;
+        const kelvinValue = answerValue + 273.15;
+        console.log(fahrenheitValue, "F");
+        console.log(kelvinValue, "K");
+      } else if (typeOfDegrees === "f") {
+        const celciusValue = (answerValue - 32) / 1.8;
+        const kelvinValue = celciusValue + 273.15;
+        console.log(celciusValue, "C");
+        console.log(kelvinValue, "K");
+      } else if (typeOfDegrees === "k") {
+        const celciusValue = (answerValue - 32) / 1.8;
+        const fahrenheitValue = answerValue * 1.8 + 32;
+        console.log(celciusValue, "C");
+        console.log(fahrenheitValue, "F");
+      } else {
+        console.error("You entered unknown type of degrees");
+      }
+    } else {
+      console.error("You didn't enter type of degrees");
+    }
   } else {
-    console.error("The entered value must be a number");
+    console.error("You entered an invalid value");
   }
 
   readlineInstance.close();
